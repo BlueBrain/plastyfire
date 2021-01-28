80fef2336ffc21c843962fcf2304de053864def1
Ecker András <andras.ecker@epfl.ch>
2021-01-28 18:39:21 +0100

    BCWriter added

--- plastyfire/__init__.py
+++ plastyfire/__init__.py
@@ -1,6 +1,7 @@
 """plastyfire"""
 
 from plastyfire.version import __version__
+from plastyfire.bcwriter import BCWriter
 from plastyfire.epg import ParamsGenerator
 from plastyfire.synapse import Synapse
 from plastyfire.generalization import SonataWriter
