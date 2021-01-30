9c3478828db2a72e35852fe5bad3160e5c183a5d
Ecker András <andras.ecker@epfl.ch>
2021-01-30 14:51:50 +0100

    updated with custom mod file path

--- setupenv.sh
+++ setupenv.sh
@@ -2,10 +2,10 @@
 
 module purge
 module load archive/2020-12
-module load neurodamus-neocortex/1.1-3.0.2
+module load neurodamus-neocortex/1.2-3.1.0
 
 source /gpfs/bbp.cscs.ch/project/proj96/home/ecker/dev-plastyfire/bin/activate
 
-#unset PMI_RANK
+CUSTOM_ND_ROOT=/gpfs/bbp.cscs.ch/project/proj96/home/ecker/gerrit
+export BGLIBPY_MOD_LIBRARY_PATH=$CUSTOM_ND_ROOT/x86_64/libnrnmech.so
 export BGLIBPY_RNG_MODE=Random123
-#export PYTHONWARNINGS="ignore"
