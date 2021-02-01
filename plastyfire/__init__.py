f9410c4ff71cf9735b94c81083e96ab51d61ee1e
Ecker András <andras.ecker@epfl.ch>
2021-02-01 16:07:01 +0100

    updating simulator

--- plastyfire/__init__.py
+++ plastyfire/__init__.py
@@ -4,4 +4,5 @@ from plastyfire.version import __version__
 from plastyfire.bcwriter import BCWriter
 from plastyfire.epg import ParamsGenerator
 from plastyfire.synapse import Synapse
-from plastyfire.generalization import SonataWriter
+from plastyfire.simulator import c_pre_finder, c_post_finder
+from plastyfire.sonatawriter import SonataWriter
