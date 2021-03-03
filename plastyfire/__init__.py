0f2843a0283320cf83706e64f86b2835ae9a4d20
Ecker András <andras.ecker@epfl.ch>
2021-03-03 14:17:57 +0100

    started refactoring the codebase

--- plastyfire/__init__.py
+++ plastyfire/__init__.py
@@ -2,7 +2,9 @@
 
 from plastyfire.version import __version__
 from plastyfire.bcwriter import BCWriter
+from plastyfire.simwriter import SimWriter
+from plastyfire.sonatawriter import SonataWriter
 from plastyfire.epg import ParamsGenerator
 from plastyfire.synapse import Synapse
-from plastyfire.simulator import c_pre_finder, c_post_finder
-from plastyfire.sonatawriter import SonataWriter
+from plastyfire.simulator import spike_threshold_finder, c_pre_finder, c_post_finder
+
