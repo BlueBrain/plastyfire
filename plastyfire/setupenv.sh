068c5c0a9d6279e05df8385dac1caed8d3b5edd1
Ecker András <andras.ecker@epfl.ch>
2021-03-16 18:10:04 +0100

    custom mod file path fixed

--- plastyfire/setupenv.sh
+++ plastyfire/setupenv.sh
@@ -6,7 +6,7 @@ module load neurodamus-neocortex/1.2-3.1.0
 
 source /gpfs/bbp.cscs.ch/project/proj96/home/ecker/dev-plastyfire/bin/activate
 
-unset BGLIBPY_MOD_LIBRARY_PATH
-#CUSTOM_ND_ROOT=/gpfs/bbp.cscs.ch/project/proj96/home/ecker/plastyfire/plastyfire
-#export BGLIBPY_MOD_LIBRARY_PATH=$CUSTOM_ND_ROOT/x86_64/libnrnmech.so
 export BGLIBPY_RNG_MODE=Random123
+unset BGLIBPY_MOD_LIBRARY_PATH
+CUSTOM_ND_ROOT=/gpfs/bbp.cscs.ch/project/proj96/home/ecker/plastyfire/plastyfire
+export BGLIBPY_MOD_LIBRARY_PATH=$CUSTOM_ND_ROOT/x86_64/libnrnmech.so
