6d25a5b156485be202facfe4c9c78565b9272846
Ecker András <andras.ecker@epfl.ch>
2021-06-09 13:47:36 +0200

    minor changes

--- plastyfire/__init__.py
+++ plastyfire/__init__.py
@@ -2,8 +2,6 @@
 
 from plastyfire.version import __version__
 from plastyfire.bcwriter import BCWriter
-from plastyfire.simwriter import SimWriter
-from plastyfire.sonatawriter import SonataWriter
 from plastyfire.epg import ParamsGenerator
 from plastyfire.synapse import Synapse
 from plastyfire.simulator import spike_threshold_finder, c_pre_finder, c_post_finder, imp_finder
