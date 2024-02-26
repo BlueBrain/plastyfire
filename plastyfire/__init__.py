83320088e566bc06cc44724feb44ee05f66f22db
Ecker András <andras.ecker@epfl.ch>
2024-02-26 11:43:12 +0100

    minor changes

--- plastyfire/__init__.py
+++ plastyfire/__init__.py
@@ -2,8 +2,6 @@
 
 from plastyfire.version import __version__
 from plastyfire.config import Config
-from plastyfire.bcwriter import BCWriter
 from plastyfire.epg import ParamsGenerator
-from plastyfire.synapse import Synapse
 from plastyfire.simulator import spike_threshold_finder, c_pre_finder, c_post_finder
 
