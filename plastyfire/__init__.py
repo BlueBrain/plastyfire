4091ae8f5423e49ca8a6fd76935b8ccf35997619
Ecker András <andras.ecker@epfl.ch>
2024-05-31 20:25:40 +0200

    added new classes

--- plastyfire/__init__.py
+++ plastyfire/__init__.py
@@ -1,7 +1,9 @@
 """plastyfire"""
 
 from plastyfire.version import __version__
-from plastyfire.config import Config
+from plastyfire.config import Config, OptConfig
 from plastyfire.epg import ParamsGenerator
-from plastyfire.simulator import spike_threshold_finder, c_pre_finder, c_post_finder
+from plastyfire.simwriter import SimWriter, OptSimWriter
+from plastyfire.ephysutils import Experiment
+from plastyfire.simulator import spike_threshold_finder, c_pre_finder, c_post_finder, runconnectedpair
 
